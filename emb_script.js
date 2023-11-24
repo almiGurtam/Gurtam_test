b7e6b853c19eab4ec73708febbc8c767f87cc04c
almiGurtam <151894245+almiGurtam@users.noreply.github.com>
2023-11-24 07:21:56 +0400

    Update and rename emb_script.js to widget.js.js

--- emb_script.js
+++
@@ -1,1 +0,0 @@
-<script type="text/javascript" id="zsiqchat">var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://zzz.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);</script>
